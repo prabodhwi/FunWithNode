68c936986207ce2dd85419c6705a20f21310c100
Prabodh Wijesinghe <Prabodh.Wijesinghe@communities.wa.gov.au>
2020-11-13 12:40:48 +0800

    This is my first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+console.log('Hello world');
+
+var x = 10;
+var y = 15;
+console.log(x * y);
+
+//var msg = require("./msgs.js");
+//console.log(msg.first);
+//console.log(msg.second);
+//console.log(msg());
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Hello World!');
+  }).listen(8080);
---
+++ msgs.js
@@ -0,0 +1,6 @@
+//exports.first = "This is the first message";
+//exports.second = "This is the second message";
+
+module.exports=function(){
+    return "This is the first message";
+};
